6231125b2c82dfa31fb86cf8cfbfed2fb1a4ce7e
EtanaAlemu <etanaalemunew@gmail.com>
2023-09-16 10:22:45 -0700

    first commit

---
+++ components/index.ts
@@ -0,0 +1,9 @@
+import Hero from './Hero'
+import CustomButton from './CustomButton'
+import Footer from './Footer'
+import CustomFilter from "./CustomFilter";
+import SearchBar from "./Searchbar";
+import CarCard from "./CarCard";
+import ShowMore from "./ShowMore"
+
+export {Hero,CustomButton,CustomFilter,SearchBar,ShowMore, CarCard,Footer}
